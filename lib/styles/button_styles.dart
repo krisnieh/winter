3e651a4165dd94389b03e3e7efd1bdc25e3b660a
kris <kris@viirose.com>
2024-12-19 17:15:49 +0800

    Update button_styles.dart

--- lib/styles/button_styles.dart
+++ lib/styles/button_styles.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 
 class ButtonStyles {
   static final ButtonStyle numericKeypadButton = ElevatedButton.styleFrom(
-    minimumSize: const Size(100, 100), // Set the button size
-    padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0), // Set the text padding
+    // minimumSize: const Size(100, 100), // Set the button size
+    // padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0), // Set the text padding
     textStyle: const TextStyle(fontSize: 24),
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(8.0),
@@ -14,8 +14,8 @@ class ButtonStyles {
   static final ButtonStyle deleteButton = ElevatedButton.styleFrom(
     foregroundColor: Colors.white,
     backgroundColor: Colors.pink,
-    minimumSize: const Size(100, 100), // Set the button size
-    padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0), // Set the text padding
+    // minimumSize: const Size(100, 100), // Set the button size
+    // padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0), // Set the text padding
     textStyle: const TextStyle(fontSize: 24),
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(8.0),
@@ -25,8 +25,8 @@ class ButtonStyles {
   static final ButtonStyle confirmButton = ElevatedButton.styleFrom(
     foregroundColor: Colors.white,
     backgroundColor: Colors.lightGreen,
-    minimumSize: const Size(100, 100), // Set the button size
-    padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0), // Set the text padding
+    // minimumSize: const Size(100, 100), // Set the button size
+    // padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0), // Set the text padding
     textStyle: const TextStyle(fontSize: 24),
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(8.0),
