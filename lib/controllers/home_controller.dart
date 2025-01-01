bc68090ad862f0fde9e9f050d0e6c9956752bfe4
Kris <kris@viirose.com>
2025-01-01 14:23:12 +0800

    init

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -11,35 +11,77 @@ class HomeController extends GetxController {
     update();
   }
 
+    Future<void> toggleError() async {
+    final NetworkController networkController = Get.find();
+    String hostname = networkController.hostname.value;
+    String url;
+
+    List<String> lists = hostname.split('-');
+
+    String lineName = lists[lists.length - 2];
+    String unitId = lists.last;
+    
+    switch (lineName) {
+      case 'a':
+        url = 'http://172.16.21.1:5000/action/';
+        break;
+      case 'b':
+        url = 'http://172.16.21.2:5000/action/';
+        break;
+      case 'c':
+        url = 'http://172.16.21.3:5000/action/';
+        break;
+      case 'd':
+        url = 'http://172.16.21.4:5000/action/';
+        break;
+      default:
+        url = 'http://172.16.0.8:5000/action/';
+    }
+    
+    url += "error/$unitId";
+
+
+    try {
+      final response = await Dio().get(url);
+      if (response.statusCode == 200) {
+        data.value = response.data.toString();
+      } else {
+        print('Failed to load data');
+      }
+    } catch (e) {
+      print('Error fetching data: $e');
+    }
+  }
+
   Future<void> toggleLights() async {
     final NetworkController networkController = Get.find();
     String hostname = networkController.hostname.value;
     String url;
 
-    // List<String> lists = hostname.split('-');
+    List<String> lists = hostname.split('-');
 
-    // String lineName = lists[lists.length - 2];
-    // String unitId = lists.last;
-    //
-    // switch (lineName) {
-    //   case 'a':
-    //     url = 'http://172.16.21.1:5000/action/';
-    //     break;
-    //   case 'b':
-    //     url = 'http://172.16.21.2:5000/action/';
-    //     break;
-    //   case 'c':
-    //     url = 'http://172.16.21.3:5000/action/';
-    //     break;
-    //   case 'd':
-    //     url = 'http://172.16.21.4:5000/action/';
-    //     break;
-    //   default:
-    //     url = 'http://172.16.0.8:5000/action/';
-    // }
-    //
-    // url += "lights/$lineName/$unitId";
-    url = 'http://172.16.21.1:5000/action/lights/1';
+    String lineName = lists[lists.length - 2];
+    String unitId = lists.last;
+    
+    switch (lineName) {
+      case 'a':
+        url = 'http://172.16.21.1:5000/action/';
+        break;
+      case 'b':
+        url = 'http://172.16.21.2:5000/action/';
+        break;
+      case 'c':
+        url = 'http://172.16.21.3:5000/action/';
+        break;
+      case 'd':
+        url = 'http://172.16.21.4:5000/action/';
+        break;
+      default:
+        url = 'http://172.16.0.8:5000/action/';
+    }
+    
+    url += "lights/$unitId";
+
 
     try {
       final response = await Dio().get(url);
