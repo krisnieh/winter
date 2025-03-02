b4d85a0de7c37f0a420519eb96e1181d6c3ab1cc
Kris <kris@viirose.com>
2025-03-02 16:07:36 +0800

    init

--- lib/components/navigation/bottom_control_bar.dart
+++ lib/components/navigation/bottom_control_bar.dart
@@ -10,7 +10,7 @@ class BottomControlBar extends StatelessWidget {
     final controller = Get.find<WorkingLineDeviceController>();
 
     return Container(
-      padding: const EdgeInsets.all(5),
+      padding: const EdgeInsets.fromLTRB(20, 5, 5, 5),
       decoration: BoxDecoration(
         color: Colors.grey[50],
         border: Border(top: BorderSide(color: Colors.grey[300]!)),
@@ -53,21 +53,46 @@ class BottomControlBar extends StatelessWidget {
               child: Obx(() => Row(
                 children: [
                   Text(
-                    '操作台: ${controller.wlSliderValue.value.round()}%  ',
-                    style: TextStyle(color: Colors.grey[600])
+                    '工作台: ${controller.wlSliderValue.value.round()}%  ',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 18,
+                    )
                   ),
                   Text(
-                    '灯光: ${controller.isWLLightOn.value ? "开启" : "关闭"}  ',
-                    style: TextStyle(color: Colors.grey[600])
+                    '| 灯光: ${controller.isWLLightOn.value ? "开启" : "关闭"}  ',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 18,
+                    )
                   ),
-                  Text('风扇: -  ', 
-                    style: TextStyle(color: Colors.grey[600])
+                  Text(
+                    '| 风扇: 未启用  ', 
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 18,
+                    )
                   ),
-                  Text('环境湿度: -  ', 
-                    style: TextStyle(color: Colors.grey[600])
+                  Text(
+                    '| 环境湿度: -  ', 
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 18,
+                    )
                   ),
-                  Text('环境温度: -', 
-                    style: TextStyle(color: Colors.grey[600])
+                  Text(
+                    '| 环境温度: -', 
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 18,
+                    )
+                  ),
+                  Text(
+                    '| 环境光亮: -', 
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 18,
+                    )
                   ),
                 ],
               )),
